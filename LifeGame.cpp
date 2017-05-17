6b85ce91f5af89ef805bf1d850d90f57c5ec44dc
asantonenko <asantonenko@gmail.com>
2017-05-17 22:17:50 +0300

    Created LifeGame class template.

---
+++ LifeGame.cpp
@@ -0,0 +1,23 @@
+#include "LifeGame.h"
+
+LifeGame::LifeGame()
+{
+    //ctor
+}
+
+LifeGame::~LifeGame()
+{
+    //dtor
+}
+
+/*LifeGame::LifeGame(const LifeGame& other)
+{
+    //copy ctor
+}
+
+LifeGame& LifeGame::operator=(const LifeGame& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}*/
---
+++ LifeGame.h
@@ -0,0 +1,16 @@
+#ifndef LIFEGAME_H
+#define LIFEGAME_H
+
+
+class LifeGame
+{
+    public:
+        LifeGame();
+        ~LifeGame();
+        //LifeGame(const LifeGame& other);
+        //LifeGame& operator=(const LifeGame& other);
+    protected:
+    private:
+};
+
+#endif // LIFEGAME_H
